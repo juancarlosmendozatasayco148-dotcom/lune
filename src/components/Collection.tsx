295e9aa06385ce8c88964bc984a75ba8947a8b7c
juancarlosmendozatasayco148-dotcom <juancarlosmendozatasayco148-dotcom@users.noreply.github.com>
2026-06-26 21:57:39 -0500

    swap Pantalon and Vestido positions in grid

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -121,8 +121,8 @@ export default function Collection() {
           <ProductCard product={products[0]} i={0} span />
           <ProductCard product={products[1]} i={1} />
           <ProductCard product={products[4]} i={4} />
-          <ProductCard product={products[2]} i={2} />
           <ProductCard product={products[3]} i={3} span />
+          <ProductCard product={products[2]} i={2} />
           <ProductCard product={products[5]} i={5} />
         </div>
       </div>
